a3777c55aac8a6ce960fb38e6b81a77c460ccc6e
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-12-23 19:48:26 +0100

    Changing of the port-keepers

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static(__dirname + '/client/build'));
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3001, () => console.log('Listening on port 3000!'));
