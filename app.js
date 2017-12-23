ef468bbaa81b236f39f5ddc067ad02d8db077a0f
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-12-23 20:34:49 +0100

    woo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/client/build'));
+app.use(express.static('./client/build'));
 
 app.listen(3001, () => console.log('Listening on port 3001!'));
