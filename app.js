8b9b9733337a3cba51a97e24a0c05ae5fee43653
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-12-23 21:16:36 +0100

    path problems

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 app.listen(3001, () => console.log('Listening on port 3001!'));
